74c83156169eb951649d300052231e99eb8fe72f
poornima-whjr <73928793+poornima-whjr@users.noreply.github.com>
2023-02-04 20:35:39 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,19 @@
 function setup() {
   createCanvas(windowWidth,windowHeight);
   box=createSprite(width/2, height/2, 200, 200);
-  box.shapeColor="red"
+  box.shapeColor="blue"
   alert("hi2")
 }
 
 function draw() {
   background(255,255,255);  
-  if(touches.length>0 && touches[i].x>box.x-100 && touches[i].x<box.x+100 && touches[i].y>box.y-100 && touches[i]<box.y+100){
+  if(touches.length>0){
+    
     alert(touches[0].x)
+    
+    if(touches[i].x>box.x-100 && touches[i].x<box.x+100 && touches[i].y>box.y-100 && touches[i]<box.y+100){
+      alert("pressed")
+    }
     touches = []
   }
   
