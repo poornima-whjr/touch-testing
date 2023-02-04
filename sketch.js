3190764b82b17aa1be11deec3348b084422c4760
poornima-whjr <73928793+poornima-whjr@users.noreply.github.com>
2023-02-04 20:47:49 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,22 +1,44 @@
+var waterCollected = 0
+
 function setup() {
-  createCanvas(windowWidth,windowHeight);
-  box=createSprite(width/2, height/2, 200, 200);
-  box.shapeColor="blue"
+  createCanvas(windowWidth, windowHeight);
+  database = firebase.database();
+  box = createSprite(width / 2, height / 2, 200, 200);
+  box.shapeColor = "yellow"
   alert("hi2")
+
+  var schoolRef = database.ref("school1/");
+  schoolRef.on("value", readWater, showError);
 }
 
 function draw() {
-  background(255,255,255);  
-  if(touches.length>0){
-    
-    alert(touches[0].x)
-    
-    if(touches[i].x>box.x-100 && touches[i].x<box.x+100 && touches[i].y>box.y-100 && touches[i]<box.y+100){
+  background(255, 255, 255);
+  if (touches.length > 0) {
+
+    alert("touch x : " + touches[0].x)
+    alert("box.x :" + box.x)
+    alert("box.y :" + box.y)
+    alert("touch y :" + touches[0].y)
+
+    if (touches[i].x > box.x - 100 && touches[i].x < box.x + 100 && touches[i].y > box.y - 100 && touches[i].y < box.y + 100) {
       alert("pressed")
     }
+
     touches = []
   }
-  
+
   drawSprites();
-  
+
+}
+
+function writeWater(water) {
+  database.ref('/').set({
+    'school1': water,
+  })
+}
+
+function readWater(data) {
+  waterCollected = data.val();
+  alert(waterCollected);
+
 }
