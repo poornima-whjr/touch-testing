9b21756631b01ca55568337829e986f13e39753e
poornima-whjr <73928793+poornima-whjr@users.noreply.github.com>
2023-02-04 20:03:48 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,13 @@
 function setup() {
   createCanvas(windowWidth,windowHeight);
   createSprite(width/2, height/2, 50, 50);
+  alert("hi")
 }
 
 function draw() {
   background(255,255,255);  
   if(touches.length>0)
-    console.log(touches[0])
+    alert(touches[0])
   
   drawSprites();
   
